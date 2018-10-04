01f3e1f02efebfc49c1c57396ef4ed0d93678587
MarsMellow <mpzwiers@gmail.com>
2018-10-04 08:56:10 +0200

    Minor documentation improvement

--- bidscoiner/dicomsort.py
+++ bidscoiner/dicomsort.py
@@ -83,7 +83,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(formatter_class=CustomFormatter,
                                      description=textwrap.dedent(__doc__),
                                      epilog='examples:\n  dicomsort.py /project/3022026.01/raw\n  dicomsort.py /project/3022026.01/raw --subjectid sub\n  dicomsort.py /project/3022026.01/raw --subjectid sub --sessionid ses\n ')
-    parser.add_argument('rawfolder',   help='The root folder containing the source [sub/][ses/]dicomfiles')
+    parser.add_argument('rawfolder',   help='The root folder containing the rawfolder/[sub/][ses/]dicomfiles')
     parser.add_argument('--subjectid', help='The prefix of the subject folders in rawfolder to search in (e.g. "sub")')
     parser.add_argument('--sessionid', help='The prefix of the session folders in the subject folder to search in (e.g. "ses")')
     parser.add_argument('--pattern',   help='The regular expression pattern used in re.match(pattern, dicomfile) to select the dicom files', default='.*\.(IMA|dcm)$')
