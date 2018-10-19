d70ed9afe7fab9b6e9b29e2640dede6f3d5bcaf3
MarsMellow <mpzwiers@gmail.com>
2018-10-19 15:13:27 +0200

    Fix for distributing the foldertree

--- setup.py
+++ setup.py
@@ -1,25 +1,23 @@
 #!/usr/bin/env python
 
-import os
-from setuptools import setup, find_packages
+from os import path
 from glob import glob
+from setuptools import setup, find_packages
 
 # Read the version from bids.py
-with open(os.path.join(os.path.dirname(__file__), 'bidscoin', 'version.txt')) as fid:
+with open(path.join(path.dirname(__file__), 'bidscoin', 'version.txt')) as fid:
     version = fid.read().strip()
 
 # Read the contents of the README file
-with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md'), encoding='utf-8') as fid:
+with open(path.join(path.abspath(path.dirname(__file__)), 'README.md'), encoding='utf-8') as fid:
     long_description = fid.read()
 
 # Read the contents of the requirements file
-with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'requirements.txt')) as fid:
+with open(path.join(path.abspath(path.dirname(__file__)), 'requirements.txt')) as fid:
     requirements = fid.read().splitlines()
 
 # Map the heuristics/sample folders
-# sample_files = glob('heuristics/**/.placeholder', recursive=True)
-sample_files = [sample_file.split(os.sep, maxsplit=1)[1] for sample_file in glob('heuristics/**/.placeholder', recursive=True)]
-print(sample_files)
+sample_files = [sample_file.split(path.sep, maxsplit=1)[1] for sample_file in glob('heuristics'+path.sep+'**'+path.sep+'.placeholder', recursive=True)]
 
 setup(name                           = 'bidscoin',                          # Required
       version                        = version,                             # Required
@@ -29,7 +27,7 @@ setup(name                           = 'bidscoin',                          # Re
                                         'heuristics': sample_files,
                                         '': ['*.yaml']},
       include_package_data           = True,
-      scripts                        = ['bidscoin/bidstrainer.py', 'bidscoin/bidsmapper.py', 'bidscoin/bidscoiner.py', 'bidscoin/rawmapper.py', 'bidscoin/dicomsort.py'],
+      scripts                        = ['bidscoin'+path.sep+'bidstrainer.py', 'bidscoin'+path.sep+'bidsmapper.py', 'bidscoin'+path.sep+'bidscoiner.py', 'bidscoin'+path.sep+'rawmapper.py', 'bidscoin'+path.sep+'dicomsort.py'],
       python_requires                = '>=3',
       classifiers                    = ['Programming Language :: Python :: 3',
                                         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
