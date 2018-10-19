f22fd7efca87023615d34dd05f20efde56ba3939
MarsMellow <mpzwiers@gmail.com>
2018-10-19 14:53:38 +0200

    Fix for distributing the foldertree

--- setup.py
+++ setup.py
@@ -1,29 +1,33 @@
 #!/usr/bin/env python
 
+import os
 from setuptools import setup, find_packages
-from os import path
 from glob import glob
 
 # Read the version from bids.py
-with open(path.join(path.dirname(__file__), 'bidscoin', 'version.txt')) as fid:
+with open(os.path.join(os.path.dirname(__file__), 'bidscoin', 'version.txt')) as fid:
     version = fid.read().strip()
 
 # Read the contents of the README file
-with open(path.join(path.abspath(path.dirname(__file__)), 'README.md'), encoding='utf-8') as fid:
+with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md'), encoding='utf-8') as fid:
     long_description = fid.read()
 
 # Read the contents of the requirements file
-with open(path.join(path.abspath(path.dirname(__file__)), 'requirements.txt')) as fid:
+with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'requirements.txt')) as fid:
     requirements = fid.read().splitlines()
 
 # Map the heuristics/sample folders
-sample_files = glob('../**/.placeholder', recursive=True)
+# sample_files = glob('heuristics/**/.placeholder', recursive=True)
+sample_files = [sample_file.split(os.sep, maxsplit=1)[1] for sample_file in glob('heuristics/**/.placeholder', recursive=True)]
+print(sample_files)
 
 setup(name                           = 'bidscoin',                          # Required
       version                        = version,                             # Required
       packages                       = find_packages(),                     # Required
       install_requires               = requirements,
-      package_data                   = {'bidscoin': sample_files.append('version.txt')},
+      package_data                   = {'bidscoin': ['version.txt'],
+                                        'heuristics': sample_files,
+                                        '': ['*.yaml']},
       include_package_data           = True,
       scripts                        = ['bidscoin/bidstrainer.py', 'bidscoin/bidsmapper.py', 'bidscoin/bidscoiner.py', 'bidscoin/rawmapper.py', 'bidscoin/dicomsort.py'],
       python_requires                = '>=3',
