8c2d4aee443914edb46388f0577d3dfb7153ac23
MarsMellow <mpzwiers@gmail.com>
2019-05-22 15:58:03 +0200

    Made proper optional arguments from the optional keyword argument

--- bidscoin/bidstrainer.py
+++ bidscoin/bidstrainer.py
@@ -271,10 +271,13 @@ if __name__ == "__main__":
                                      epilog='examples:\n' 
                                             '  bidstrainer.py /project/foo/bids\n' 
                                             '  bidstrainer.py /project/foo/bids /project/foo/samples bidsmap_custom\n ')
-    parser.add_argument('bidsfolder',     help='The destination folder with the bids data structure')
-    parser.add_argument('samplefolder',   help='The root folder of the directory tree containing the sample files / training data. Optional argument, if left empty, bidsfolder/code/samples is used or such an empty directory tree is created', nargs='?', default='')
-    parser.add_argument('bidsmap',        help='The bidsmap YAML-file with the BIDS heuristics (optional argument, default: ./heuristics/bidsmap_template.yaml)', nargs='?', default='bidsmap_template.yaml')
-    parser.add_argument('-p','--pattern', help='The regular expression pattern used in re.match(pattern, dicomfile) to select the dicom files', default=r'.*\.(IMA|dcm)$')
+    parser.add_argument('bidsfolder',           help='The destination folder with the bids data structure')
+    parser.add_argument('-s','--samplefolder',  help='The root folder of the directory tree containing the sample files / training data. By default the bidsfolder/code/samples folder is used or such an empty directory tree is created', default='')
+    parser.add_argument('-b','--bidsmap',       help='The bidsmap YAML-file with the BIDS heuristics (default: ./heuristics/bidsmap_template.yaml)', default='bidsmap_template.yaml')
+    parser.add_argument('-p','--pattern',       help='The regular expression pattern used in re.match(pattern, dicomfile) to select the dicom files', default=r'.*\.(IMA|dcm)$')
     args = parser.parse_args()
 
-    bidstrainer(bidsfolder=args.bidsfolder, samplefolder=args.samplefolder, bidsmapfile=args.bidsmap, pattern=args.pattern)
+    bidstrainer(bidsfolder   = args.bidsfolder,
+                samplefolder = args.samplefolder,
+                bidsmapfile  = args.bidsmap,
+                pattern      = args.pattern)
