d63aa0b371566c1505387d9492586b3c9baf719e
MarsMellow <mpzwiers@gmail.com>
2019-08-08 16:10:39 +0200

    Code clean-up for missing AcquisitionTime field in the dcm2niix json file (due to e.g. anonymization)

--- bidscoin/bidscoiner.py
+++ bidscoin/bidscoiner.py
@@ -247,13 +247,12 @@ def coin_dicom(session: str, bidsmap: dict, bidsfolder: str, personals: dict, su
             # Parse the acquisition time from the json file
             with open(jsonfile, 'r') as json_fid:
                 data = json.load(json_fid)
-            if 'AcquisitionTime' in data:
-                acq_time = dateutil.parser.parse(data['AcquisitionTime']).strftime('%H:%M:%S')
-            else:
-                acq_time = bids.get_dicomfield('AcquisitionTime', dicomfile)
+            if 'AcquisitionTime' not in data:
+                data['AcquisitionTime'] = bids.get_dicomfield('AcquisitionTime', dicomfile)
+            acq_time = dateutil.parser.parse(data['AcquisitionTime'])
             niipath  = glob.glob(os.path.splitext(jsonfile)[0] + '.nii*')[0]    # Find the corresponding nifti file (there should be only one, let's not make assumptions about the .gz extension)
             niipath  = niipath.replace(bidsses+os.sep,'')                       # Use a relative path. Somehow .strip(bidsses) instead of replace(bidsses,'') does not work properly
-            scans_table.loc[niipath, 'acq_time'] = '1900-01-01T' + acq_time
+            scans_table.loc[niipath, 'acq_time'] = '1900-01-01T' + acq_time.strftime('%H:%M:%S')
 
     # Write the scans_table to disk
     LOGGER.info('Writing acquisition time data to: ' + scans_tsv)
