ea9ddfb414158a724b6feaa9bc5bd3371d3bd54a
MarsMellow <mpzwiers@gmail.com>
2019-08-29 16:19:47 +0200

    Slightly improved logging

--- bidscoin/bidscoiner.py
+++ bidscoin/bidscoiner.py
@@ -258,16 +258,15 @@ def coin_dicom(session: str, bidsmap: dict, bidsfolder: str, personals: dict, su
                                      for niifile in sorted(glob.glob(os.path.join(bidsses, f'**{os.sep}*{selector}*.nii*'))) if selector])      # Search in all runs using a relative path
             if len(niifiles)<=1:
                 niifiles = ''.join(niifiles)                                                                                                    # Only use a list for more than 1 file
+            if not niifiles:
+                LOGGER.warning(f"Empty 'IntendedFor' fieldmap value in {os.path.join(bidsses)}: the search for {intendedfor} from the bidsmap gave no results")
 
             # Save the IntendedFor data in the json-files (account for multiple runs and dcm2niix suffixes inserted into the acquisition label)
             acqlabel = bids.get_bidsvalue(bidsname, 'acq')
             for jsonfile in glob.glob(os.path.join(bidsses, 'fmap', bidsname.replace('_run-1_', '_run-[0-9]*_') + '.json')) + \
                             glob.glob(os.path.join(bidsses, 'fmap', bidsname.replace('_run-1_', '_run-[0-9]*_').replace(acqlabel, acqlabel+'[CE][0-9]*') + '.json')):
 
-                if not niifiles:
-                    LOGGER.warning(f"Empty 'IntendedFor' fieldmap value in {jsonfile}: the search for {intendedfor} from the bidsmap gave no results")
-                else:
-                    LOGGER.info(f'Adding IntendedFor to: {jsonfile}')
+                LOGGER.info(f'Adding IntendedFor to: {jsonfile}')
                 with open(jsonfile, 'r') as json_fid:
                     data = json.load(json_fid)
                 data['IntendedFor'] = niifiles
