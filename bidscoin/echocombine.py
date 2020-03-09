78e6239e3d0d9036716f14993bbae776e45d6e2a
MarsMellow <mpzwiers@gmail.com>
2020-03-09 14:53:09 +0100

    Make EchoTime = EchoNumer = '' in the combined-echo json sidecar file instead of [] (which gives an error)

--- bidscoin/echocombine.py
+++ bidscoin/echocombine.py
@@ -97,8 +97,8 @@ def echocombine(bidsdir: str, pattern: str, subjects: list, output: str, algorit
                     shutil.copyfile(sejson, cejson)
                     with cejson.open('r') as fmap_fid:
                         data = json.load(fmap_fid)
-                    data['EchoTime']   = []
-                    data['EchoNumber'] = []
+                    data['EchoTime']   = ''
+                    data['EchoNumber'] = ''
                     with cejson.open('w') as fmap_fid:
                         json.dump(data, fmap_fid, indent=4)
 
