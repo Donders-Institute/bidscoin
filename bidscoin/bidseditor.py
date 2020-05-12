9c202fe7fa61d7ebc740e0c7d7f68e6a99faef24
MarsMellow <mpzwiers@gmail.com>
2020-05-12 20:57:13 +0200

    QDialog improvement -> remove obsolete message to help the user when exporting to a non-existent file

--- bidscoin/bidseditor.py
+++ bidscoin/bidseditor.py
@@ -1304,18 +1304,14 @@ class EditDialog(QDialog):
 
     def export_run(self):
 
-        yamlfile, _ = QFileDialog.getSaveFileName(self, 'Export run item to (template) bidsmap',
+        yamlfile, _ = QFileDialog.getOpenFileName(self, 'Export run item to (template) bidsmap',
                         str(bids.bidsmap_template), 'YAML Files (*.yaml *.yml);;All Files (*)')
         if yamlfile:
-            yamlfile = Path(yamlfile)
-            if yamlfile.is_file():
-                LOGGER.info(f'Exporting run item: bidsmap[{self.dataformat}][{self.target_modality}] -> {yamlfile}')
-                bidsmap, _ = bids.load_bidsmap(yamlfile, Path(), False)
-                bidsmap    = bids.append_run(bidsmap, self.dataformat, self.target_modality, self.target_run)
-                bids.save_bidsmap(yamlfile, bidsmap)
-            else:
-                QMessageBox.critical(self, 'Export run item', f"Cannot export to:\n\n{yamlfile}\n\nPlease select an existing bidsmap yaml-file")
-                self.export_run()
+            LOGGER.info(f'Exporting run item: bidsmap[{self.dataformat}][{self.target_modality}] -> {yamlfile}')
+            yamlfile   = Path(yamlfile)
+            bidsmap, _ = bids.load_bidsmap(yamlfile, Path(), False)
+            bidsmap    = bids.append_run(bidsmap, self.dataformat, self.target_modality, self.target_run)
+            bids.save_bidsmap(yamlfile, bidsmap)
 
 
 def bidseditor(bidsfolder: str, bidsmapfile: str='', templatefile: str='', dataformat: str='DICOM', subprefix='sub-', sesprefix='ses-'):
