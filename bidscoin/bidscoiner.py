42e8190877307c373b728a46d9022c166212aa88
MarsMellow <mpzwiers@gmail.com>
2020-09-25 09:18:04 +0200

    Bugfix for empty `AcquisitionTime` (DICOM) and `exam_date` (PAR) fields

--- bidscoin/bidscoiner.py
+++ bidscoin/bidscoiner.py
@@ -240,9 +240,12 @@ def coin_data2bids(dataformat: str, session: Path, bidsmap: dict, bidsfolder: Pa
                     data['AcquisitionTime'] = bids.get_sourcefield('AcquisitionTime', sourcefile)       # DICOM
                 if not data['AcquisitionTime']:
                     data['AcquisitionTime'] = bids.get_sourcefield('exam_date', sourcefile)             # PAR/XML
-                acq_time = dateutil.parser.parse(data['AcquisitionTime'])
+                if data['AcquisitionTime']:
+                    acq_time = dateutil.parser.parse(data['AcquisitionTime']).strftime('%H:%M:%S')
+                else:
+                    acq_time = '00:00:00'
                 scanpath = list(jsonfile.parent.glob(jsonfile.stem + '.nii*'))[0].relative_to(bidsses)  # Find the corresponding nifti file (there should be only one, let's not make assumptions about the .gz extension)
-                scans_table.loc[scanpath.as_posix(), 'acq_time'] = '1925-01-01T' + acq_time.strftime('%H:%M:%S')
+                scans_table.loc[scanpath.as_posix(), 'acq_time'] = '1925-01-01T' + acq_time
 
     # Write the scans_table to disk
     LOGGER.info(f"Writing acquisition time data to: {scans_tsv}")
