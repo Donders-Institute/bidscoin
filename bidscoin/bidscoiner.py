dd47203e65ae5b8d4a7fe5239dcc8cf356cdeeaf
MarsMellow <mpzwiers@gmail.com>
2020-09-25 17:21:30 +0200

    Slightly tweaked log message

--- bidscoin/bidscoiner.py
+++ bidscoin/bidscoiner.py
@@ -243,7 +243,7 @@ def coin_data2bids(dataformat: str, session: Path, bidsmap: dict, bidsfolder: Pa
                 try:
                     acq_time = dateutil.parser.parse(data['AcquisitionTime'])
                 except:
-                    LOGGER.warning(f"Could not parse date-time from: '{data['AcquisitionTime']}' in {sourcefile}")
+                    LOGGER.warning(f"Could not parse the acquisition time from: '{data['AcquisitionTime']}' in {sourcefile}")
                     acq_time = dateutil.parser.parse('00:00:00')
                 scanpath = list(jsonfile.parent.glob(jsonfile.stem + '.nii*'))[0].relative_to(bidsses)  # Find the corresponding nifti file (there should be only one, let's not make assumptions about the .gz extension)
                 scans_table.loc[scanpath.as_posix(), 'acq_time'] = '1925-01-01T' + acq_time.strftime('%H:%M:%S')
