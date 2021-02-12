2862ba48cba7864f1f0f12e478e10c2a5ae46576
MarsMellow <mpzwiers@gmail.com>
2021-02-12 16:05:10 +0100

    Moving all bidscoin installed files to one root directory

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(name                           = 'bidscoin',          # Required
       python_requires                = '>=3.6',
       setup_requires                 = ["pytest-runner"],
       tests_require                  = ["pytest", "pytest-cov", "coverage"],
-      package_data                   = {'': ['../*version.txt', 'bidscoin.ico', '*.yaml']},
+      package_data                   = {'': ['*version.txt', 'bidscoin.ico', '*.yaml']},
       entry_points                   = {'console_scripts': ['bidseditor       = bidscoin.bidseditor:main',
                                                             'bidstrainer      = bidscoin.bidstrainer:main',
                                                             'bidsmapper       = bidscoin.bidsmapper:main',
