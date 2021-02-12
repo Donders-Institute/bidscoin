4f99b5ff4fe48ed5513c421d9e74288ca873d306
MarsMellow <mpzwiers@gmail.com>
2021-02-12 15:04:48 +0100

    Trying to fix MANIFEST / package_data hell

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(name                           = 'bidscoin',          # Required
       python_requires                = '>=3.6',
       setup_requires                 = ["pytest-runner"],
       tests_require                  = ["pytest", "pytest-cov", "coverage"],
-      package_data                   = {'bidscoin': ['../*version.txt', 'icons/brain.ico'],
+      package_data                   = {'bidscoin': ['../*version.txt', 'docs/_static/brain.ico'],
                                         '': ['*.yaml']},
       entry_points                   = {'console_scripts': ['bidseditor       = bidscoin.bidseditor:main',
                                                             'bidstrainer      = bidscoin.bidstrainer:main',
