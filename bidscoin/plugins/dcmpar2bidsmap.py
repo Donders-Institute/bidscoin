e1675da3891845e7202c9580108ac422e0d5c6b8
MarsMellow <mpzwiers@gmail.com>
2021-04-01 07:45:27 +0200

    Tiny bugfix

--- bidscoin/plugins/dcmpar2bidsmap.py
+++ bidscoin/plugins/dcmpar2bidsmap.py
@@ -66,7 +66,7 @@ def bidsmapper_plugin(session: Path, bidsmap_new: dict, bidsmap_old: dict, templ
                 targetfile        = store['target']/sourcefile.relative_to(store['source'])
                 targetfile.parent.mkdir(parents=True, exist_ok=True)
                 LOGGER.info(f"Storing {datatype} {dataformat} sample: {sourcefile} -> {targetfile}")
-                run['provenance'] = Path(shutil.copy2(sourcefile, targetfile))
+                run['provenance'] = str(shutil.copy2(sourcefile, targetfile))
 
             # Copy the filled-in run over to the new bidsmap
             bids.append_run(bidsmap_new, dataformat, datatype, run)
