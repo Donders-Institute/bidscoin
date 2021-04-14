25aea24ca0f97dd8290dad6a210b12a15f228aad
MarsMellow <mpzwiers@gmail.com>
2021-04-14 15:13:41 +0200

    Bugfix (missing inclusion of the '.' in a file extension)

--- bidscoin/plugins/dcm2niix2bids.py
+++ bidscoin/plugins/dcm2niix2bids.py
@@ -118,7 +118,7 @@ def bidscoiner_plugin(session: Path, bidsmap: dict, bidsfolder: Path, personals:
         # Check if file already exists (-> e.g. when a static runindex is used)
         if (outfolder/bidsname).with_suffix('.json').is_file():
             LOGGER.warning(f"{outfolder/bidsname}.* already exists and will be deleted -- check your results carefully!")
-            for ext in ('.nii.gz', '.nii', '.json', '.bval', '.bvec', 'tsv.gz'):
+            for ext in ('.nii.gz', '.nii', '.json', '.bval', '.bvec', '.tsv.gz'):
                 (outfolder/bidsname).with_suffix(ext).unlink(missing_ok=True)
 
         # Convert physiological log files (dcm2niix can't handle these)
