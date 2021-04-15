c5e4080992be9f57b5c7272a94976c58f2615b8a
MarsMellow <mpzwiers@gmail.com>
2021-04-15 09:36:21 +0200

    Added a CHANGELOG.md file

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,8 @@ with open(Path(__file__).parents[1]/'bidscoin'/'version.txt') as fid:
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['recommonmark']
-source_suffix = ['.rst', '.md']
+source_suffix = {'.rst': 'restructuredtext',
+                 '.md': 'markdown'}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
