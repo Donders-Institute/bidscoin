5b81d0807a826907bf6e932e13f92942757c8dc3
MarsMellow <mpzwiers@gmail.com>
2021-10-15 17:02:54 +0200

    Minor logging tweak

--- bidscoin/plugins/dcm2niix2bids.py
+++ bidscoin/plugins/dcm2niix2bids.py
@@ -381,7 +381,7 @@ def bidscoiner_plugin(session: Path, bidsmap: dict, bidsfolder: Path) -> None:
             for metakey, metaval in run['meta'].items():
                 if metakey != 'IntendedFor':
                     metaval = datasource.dynamicvalue(metaval, cleanup=False, runtime=True)
-                LOGGER.info(f"Adding '{metakey}: {metaval}' to: {jsonfile}")
+                    LOGGER.info(f"Adding '{metakey}: {metaval}' to: {jsonfile}")
                 jsondata[metakey] = metaval
 
             # Save the meta-data to the json sidecar-file
