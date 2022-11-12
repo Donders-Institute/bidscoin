e001c54eb7aed2ab7fe31720cacbce415e49a3c7
MarsMellow <mpzwiers@gmail.com>
2022-11-12 01:57:26 +0100

    LOGGING tweaks

--- bidscoin/bidscoin.py
+++ bidscoin/bidscoin.py
@@ -98,7 +98,7 @@ def setup_logging(logfile: Path=Path(), debug: bool=False):
 
     # Add the console streamhandler and bring some color to those boring logs! :-)
     coloredlogs.install(level='VERBOSE' if debug or not logfile.name else 'INFO', fmt=cfmt, datefmt=datefmt)   # NB: Using tqdm sets the streamhandler level to 0, see: https://github.com/tqdm/tqdm/pull/1235
-    coloredlogs.DEFAULT_LEVEL_STYLES['verbose']['color'] = 250  # = Gray
+    coloredlogs.DEFAULT_LEVEL_STYLES['verbose']['color'] = 245  # = Gray
 
     if not logfile.name:
         return
