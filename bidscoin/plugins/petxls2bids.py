e03622e17005eebde3c361313dcca66be8449430
Anthony Galassi <28850131+bendhouseart@users.noreply.github.com>
2022-11-29 11:49:28 -0600

    syncing w/ master

--- bidscoin/plugins/petxls2bids.py
+++ bidscoin/plugins/petxls2bids.py
@@ -9,9 +9,14 @@ See also:
 import logging
 import pandas as pd
 import json
+import pypet2bids as pet
+import subprocess
 from typing import Union
 from pathlib import Path
 from functools import lru_cache
+
+import pypet2bids.helper_functions
+
 try:
     from bidscoin import bidscoin, bids
 except ImportError:
@@ -25,15 +30,18 @@ OPTIONS = {'anon': 'y'}                     # Set this anonymization flag to 'y'
 
 def test(options) -> bool:
     """
-    Performs shell tests of dcm2niix
+    Performs shell tests of pypet2bids
 
     :param options: A dictionary with the plugin options, e.g. taken from the bidsmap['Options']['plugins']['dcm2niix2bids']
     :return:        True if the tool generated the expected result, False if there was a tool error
     """
 
-    LOGGER.info('Testing petxls2bids is not implemented (yet):')
-
-    return True
+    LOGGER.info('Testing petxls2bids:')
+    check = subprocess.run('dcm2niix4pet -h', capture_output=True, shell=True)
+    if check.returncode == 0:
+        return True
+    else:
+        return False
 
 
 @lru_cache(maxsize=4096)
@@ -46,8 +54,23 @@ def is_sourcefile(file: Path) -> str:
     """
 
     if file.suffix.lower().startswith('.xls'):
-        data = pd.read_excel(file)
-        if "PatientID" in data:
+        data = pet.helper_functions.single_spreadsheet_reader(file)
+        try:
+            with open(pet.helper_functions.pet_metadata_json, 'r') as pet_field_requirements_json:
+                pet_field_requirements = json.load(pet_field_requirements_json)
+        except (FileNotFoundError, json.JSONDecodeError) as error:
+            logging.error(f"Unable to load list of required, recommended, and optional PET BIDS fields from"
+                          f" {pet.helper_functions.pet_metadata_json}, will not be able to determine if {file} contains"
+                          f" PET BIDS specific metadata")
+            raise error
+
+        mandatory_fields = pet_field_requirements.get('mandatory', [])
+        recommended_fields = pet_field_requirements.get('recommended', [])
+        optional_fields = pet_field_requirements.get('optional', [])
+        intersection = set(mandatory_fields + recommended_fields + optional_fields) & set(data.keys())
+
+        # 3 seems like a reasonable amount of fields to determine whether the spreadsheet is a pet recording
+        if len(intersection) > 3:
             return 'PETXLS'
 
     return ''
@@ -65,7 +88,7 @@ def get_attribute(dataformat: str, sourcefile: Path, attribute: str, options: di
     """
     if dataformat == 'PETXLS':
 
-        data = pd.read_excel(sourcefile)
+        data = pet.helper_functions.single_spreadsheet_reader(sourcefile)
 
         return data.get(attribute)
 
@@ -98,9 +121,6 @@ def bidscoiner_plugin(session: Path, bidsmap: dict, bidsses: Path) -> None:
                 LOGGER.error(f"Found ambiguous PET meta data file: {file}")
                 return
 
-            # Load the Excel data. TODO: Discuss this with Anthony and Cyril
-            metadata = pd.read_excel(file)
-
             # Load the json sidecar data (there should be only one)
             jsonfile = sorted((bidsses/'pet').rglob('*.json'))
             if len(jsonfile) > 1:
@@ -109,10 +129,10 @@ def bidscoiner_plugin(session: Path, bidsmap: dict, bidsses: Path) -> None:
             with jsonfile[0].open('r') as json_fid:
                 jsondata = json.load(json_fid)
 
-            # Add the meta-data. TODO: implement this once we know how `metadata` is organised
-            for key in metadata:
-                jsondata[key] = metadata[key]
+            # Load the Excel data and combine it with the existing sidecar json data
+            metadata = pypet2bids.helper_functions.single_spreadsheet_reader(path_to_spreadsheet=file,
+                                                                             metadata=jsondata)
 
             # Save the meta-data to the json sidecar file
             with jsonfile[0].open('w') as json_fid:
-                json.dump(jsondata, json_fid, indent=4)
+                json.dump(metadata, json_fid, indent=4, default=str)
