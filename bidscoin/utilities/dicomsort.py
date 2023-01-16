6d12b54696d6c538df39a277b4819227ebe9ea37
MarsMellow <mpzwiers@gmail.com>
2023-01-16 10:43:51 +0100

    Loosen / fix regexp deprecated escape warning

--- bidscoin/utilities/dicomsort.py
+++ bidscoin/utilities/dicomsort.py
@@ -59,7 +59,7 @@ def validscheme(scheme: str) -> bool:
     :return:
     """
 
-    if not re.fullmatch('(({[a-zA-Z]+(:\\d+d)?})|([a-zA-Z0-9\-_.]+))*', scheme):        # TODO: fix deprecated escape sequence \-
+    if not re.fullmatch(r'(({[a-zA-Z0-9]+(:\d+d)?})|([a-zA-Z0-9_.-]+))*', scheme):
         LOGGER.error(f"Bad naming scheme: {scheme}. Only alphanumeric characters could be used for the field names (with the optional number of digits afterwards,"
                       "e.g. '{InstanceNumber:05d}'), and only alphanumeric characters, dots, and dashes + underscores could be used as separators.")
         return False
