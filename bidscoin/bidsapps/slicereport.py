1f89c2bfb02d44869df532c24fa0d5dc308c8def
MarsMellow <mpzwiers@gmail.com>
2023-01-31 23:29:14 +0100

    Added options for subreports and QC-scores

--- bidscoin/bidsapps/slicereport.py
+++ bidscoin/bidsapps/slicereport.py
@@ -11,6 +11,7 @@ import argparse
 import logging
 import subprocess
 import sys
+import csv
 from pathlib import Path
 try:
     from bidscoin import bidscoin
@@ -19,15 +20,81 @@ except ImportError:
     import bidscoin
 
 
-def slicereport(bidsdir: str, pattern: str, outlinepattern: str, outlineimage: str, subjects: list, mainopts: list, outputopts: list, reportdir: str):
+def parsemainopts(mainopts: list) -> str:
+    """Check the MAINOPTS arguments and return them to a string that can be passed to slicer"""
+    for n, mainopt in enumerate(mainopts):
+        if mainopts[n-1] == 'l': continue                       # Skip checking the LUT string
+        if not (mainopt in ('L','l','i','e','t','n','u','s','c') or mainopt.replace('.','').replace('-','').isdecimal()):
+            print(f"Invalid MAINOPTS: {' '.join(mainopts)}"); sys.exit(2)
+        if mainopt.isalpha():
+            mainopts[n] = '-' + mainopts[n]
+
+    return  f"{' '.join(mainopts)}"
+
+
+def parseoutputopts(opts: list, name: str) -> tuple:
+    """
+    Check the NAME arguments and append slicer output images to them
+
+    :return: (outputopts: str, outputimages: list)
+    """
+    sliceimages = []
+    outputopts  = ''
+    isnumber    = lambda arg: arg.replace('.','').replace('-','').isdecimal()
+    for n, outputopt in enumerate(opts):
+        if not (outputopt in ('x', 'y', 'z', 'a', 'A', 'S') or isnumber(outputopt)):
+            print(f"Invalid {name}: '{outputopt}' in '{' '.join(opts)}'"); sys.exit(2)
+        if outputopt.isalpha():
+            sliceimages.append(f"slice_tmp{n}.png")
+            if outputopt == 'a':
+                outputopts += f"-{outputopt} {sliceimages[-1]} "
+            elif outputopt in ('x', 'y', 'z', 'A'):
+                if not isnumber(opts[n+1]):
+                    print(f"Invalid {name}: '{opts[n + 1]}' in '{' '.join(opts)}'"); sys.exit(2)
+                outputopts += f"-{outputopt} {opts[n+1]} {sliceimages[-1]} "
+            elif outputopt == 'S':
+                if not (isnumber(opts[n+1]) and isnumber(opts[n+2])):
+                    print(f"Invalid {name}: {outputopt} >> '{' '.join(opts)}'"); sys.exit(2)
+                outputopts += f"-{outputopt} {opts[n+1]} {opts[n+2]} {sliceimages[-1]} "
+
+    return outputopts, sliceimages
+
+
+def appendslices(inputimage, outlineimage, mainopts, outputopts, reportses, slicerimages, outputimage):
+    """Run slicer and append the sliced images to a row"""
+
+    # Slice the image
+    command = f"slicer {inputimage} {outlineimage} {mainopts} {outputopts}"
+    LOGGER.bcdebug(f"Running: {command}")
+    process = subprocess.run(command, cwd=reportses, shell=True, capture_output=True, text=True)
+    if process.stderr or process.returncode!=0:
+        LOGGER.error(f"{command}\nErrorcode {process.returncode}:\n{process.stdout}\n{process.stderr}")
+        sys.exit(process.returncode)
+
+    # Append the sliced images to a row
+    command = f"pngappend {' + '.join(slicerimages)} {outputimage}"
+    process = subprocess.run(command, cwd=reportses, shell=True, capture_output=True, text=True)
+    if process.stderr or process.returncode!=0:
+        LOGGER.error(f"{command}\nErrorcode {process.returncode}:\n{process.stdout}\n{process.stderr}")
+        sys.exit(process.returncode)
+
+    # Clean up the intermediate slicer output images
+    for slicerimage in slicerimages:
+        (reportses/slicerimage).unlink()
+
+
+def slicereport(bidsdir: str, pattern: str, outlinepattern: str, outlineimage: str, subjects: list, reportdir: str, qccols: list, mainopts: list, outputopts: list, suboutputopts: list):
     """
     :param bidsdir:         The bids-directory with the subject data
     :param pattern:         Globlike search pattern to select the images in bidsdir to be reported, e.g. 'anat/*_T1w*'
     :param outlinepattern:  Globlike search pattern to select red-outline images that are projected on top of the reported images. Prepend `outlinedir:` if your outline images are in `outlinedir` instead of `bidsdir`
     :param outlineimage:    A common red-outline image that is projected on top of all images
+    :param subjects:        Space separated list of sub-# identifiers to be processed (the sub-prefix can be left out). If not specified then all sub-folders in the bidsfolder will be processed
+    :param reportdir:       The folder where the report is saved
+    :param qccols:          Column names for creating an accompanying tsv-file to store QC-rating scores
     :param mainopts:        Slicer main options
     :param outputopts:      Slicer output options
-    :param reportdir:       The folder where the report is saved
+    :param suboutputopts:   Slicer output options for creating the subreports (same as OUTPUTOPTS)
     :return:
     """
 
@@ -53,34 +120,10 @@ def slicereport(bidsdir: str, pattern: str, outlinepattern: str, outlineimage: s
     else:
         outlinedir = bidsdir
 
-    # Format the slicer main options
-    isnumber = lambda arg: arg.replace('.','').replace('-','').isdecimal()
-    for n, mainopt in enumerate(mainopts):
-        if mainopts[n-1] == 'l': continue     # Skip checking the LUT string
-        if not (mainopt in ('L','l','i','e','t','n','u','s','c') or isnumber(mainopt)):
-            print(f"Invalid MAINOPTS: {' '.join(mainopts)}"); return
-        if mainopt.isalpha():
-            mainopts[n] = '-' + mainopts[n]
-    mainopts = f"{' '.join(mainopts)}"
-
-    # Format the slicer output options and images
-    sliceimages = []
-    outputopts_ = ''
-    for n, outputopt in enumerate(outputopts):
-        if not (outputopt in ('x', 'y', 'z', 'a', 'A', 'S') or isnumber(outputopt)):
-            print(f"Invalid OUTPUTOPTS: '{outputopt}' in '{' '.join(outputopts)}'"); return
-        if outputopt.isalpha():
-            sliceimages.append(f"slice_tmp{n}.png")
-            if outputopt == 'a':
-                outputopts_ += f"-{outputopt} {sliceimages[-1]} "
-            elif outputopt in ('x', 'y', 'z', 'A'):
-                if not isnumber(outputopts[n+1]):
-                    print(f"Invalid OUTPUTOPTS: '{outputopts[n+1]}' in '{' '.join(outputopts)}'"); return
-                outputopts_ += f"-{outputopt} {outputopts[n+1]} {sliceimages[-1]} "
-            elif outputopt == 'S':
-                if not (isnumber(outputopts[n+1]) and isnumber(outputopts[n+2])):
-                    print(f"Invalid OUTPUTOPTS: {outputopt} >> '{' '.join(outputopts)}'"); return
-                outputopts_ += f"-{outputopt} {outputopts[n+1]} {outputopts[n+2]} {sliceimages[-1]} "
+    # Format the slicer main and output options and sliced images
+    mainopts                       = parsemainopts(mainopts)
+    outputopts, slicerimages       = parseoutputopts(outputopts, 'OUTPUTOPTS')
+    suboutputopts, subslicerimages = parseoutputopts(suboutputopts, 'SUBOUTPUTOPTS')
 
     # Get the list of subjects
     if not subjects:
@@ -99,14 +142,31 @@ def slicereport(bidsdir: str, pattern: str, outlinepattern: str, outlineimage: s
 
     # Create the report index file
     report = reportdir/'index.html'
-    report.write_text(f'<HTML><TITLE>slicereport</TITLE><BODY BGCOLOR="#181818"><h3 style="color:#FFFFFF">Command: slicereport {" ".join(sys.argv[1:])}</h3><br>')
+    report.write_text('<HTML><TITLE>slicereport</TITLE><BODY BGCOLOR="#181818">\n'
+                     f'<h3 style="color:#FFFFFF">Command: slicereport {" ".join(sys.argv[1:])}</h3><br>')
+
+    # Create a QC tsv-file
+    qcfile = reportdir/'qcscores.tsv'
+    if qccols:
+        with open(qcfile, 'wt') as fid:
+            tsv_writer = csv.writer(fid, delimiter='\t')
+            tsv_writer.writerow(['subject/session'] + qccols)
 
     # Loop over the subject/session-directories
     for subject in subjects:
         sessions = bidscoin.lsdirs(subject, 'ses-*')
         if not sessions:
             sessions = [subject]
+            subses   = ''
+        else:
+            subses   = subject.name + '/'
         for session in sessions:
+
+            # Write a row in the QC tsv-file
+            if qccols:
+                with open(qcfile, 'a') as fid:
+                    tsv_writer = csv.writer(fid, delimiter='\t')
+                    tsv_writer.writerow([str(session.relative_to(bidsdir))] + len(qccols) * ['n/a'])
 
             # Search for the image(s) to report
             LOGGER.info(f"Processing images in: {session.relative_to(bidsdir)}")
@@ -123,42 +183,44 @@ def slicereport(bidsdir: str, pattern: str, outlinepattern: str, outlineimage: s
                     outlineimages = [''] * len(images)
 
             # Generate the report row(s)
+            reportses = reportdir/session.relative_to(bidsdir)
+            reportses.mkdir(parents=True, exist_ok=True)
             for n, image in enumerate(images):
 
-                # Generate the slice image
-                outline = outlineimages[n] if outlinepattern else outlineimage
-                command = f"slicer {image} {outline} {mainopts} {outputopts_}"
-                LOGGER.bcdebug(f"Running: {command}")
-                process = subprocess.run(command, cwd=reportdir, shell=True, capture_output=True, text=True)
-                if process.stderr or process.returncode != 0:
-                    LOGGER.error(f"{command}\nErrorcode {process.returncode}:\n{process.stdout}\n{process.stderr}")
-                    continue
-
-                # Append the slice images to a slice row
+                # Generate the sliced image row
+                outline  = outlineimages[n] if outlinepattern else outlineimage
                 slicerow = f"{Path(image).name}.png"
-                command  = f"pngappend {' + '.join(sliceimages)} {slicerow}"
-                process  = subprocess.run(command, cwd=reportdir, shell=True, capture_output=True, text=True)
-                for sliceimage in sliceimages:
-                    (reportdir/sliceimage).unlink()
-                if process.stderr or process.returncode != 0:
-                    LOGGER.error(f"{command}\nErrorcode {process.returncode}:\n{process.stdout}\n{process.stderr}")
-                    continue
+                appendslices(image, outline, mainopts, outputopts, reportses, slicerimages, slicerow)
 
                 # Add a row to the report
-                caption = f"{Path(image).relative_to(bidsdir)}{'&nbsp; &nbsp; &nbsp;['+str(Path(outline).relative_to(outlinedir))+']' if outline else ''}"
+                caption = f"{Path(image).relative_to(bidsdir)}{'&nbsp;&nbsp;&nbsp;['+str(Path(outline).relative_to(outlinedir))+']' if outline else ''}"
                 with report.open('a') as fid:
-                    fid.write(f'<p style="color:#D1D1D1"><image src="{slicerow}"><br>{caption}</p>')
+                    fid.write(f'\n<p><a href="{subses + session.name}/index.html" style="color:#D1D1D1; text-decoration:inherit;">'
+                                f'<image src="{subses + session.name}/{slicerow}"><br>\n{caption}</a></p>')
+
+                # Add a subreport
+                if suboutputopts:
+                    slicerow = f"{Path(image).name}_s.png"
+                    appendslices(image, outline, mainopts, suboutputopts, reportses, subslicerimages, slicerow)
+                (reportses/'index.html').write_text('<HTML><TITLE>slicereport</TITLE><BODY BGCOLOR="#181818">\n'
+                                                   f'<h3 style="color:#FFFFFF">{caption}</h3><br>\n'
+                                                   f'<p style="color:#D1D1D1"><image src="{slicerow}">\n</BODY></HTML>')
 
     # Finish off
     errors = bidscoin.reporterrors().replace('\n', '<br>')
     if errors:
-        message = '<h3 style="color:#EE4B2B">The following errors and warnings were reported:</h3>'
+        footer = '<h3 style="color:#EE4B2B">The following errors and warnings were reported:</h3>'
     else:
-        message = '<h3 style="color:#50C878">No errors or warnings were reported</h3>'
+        footer = '<h3 style="color:#50C878">No errors or warnings were reported</h3>'
     with report.open('a') as fid:
-        fid.write(f'<br>{message}<p style="color:#D1D1D1">{errors}</p></BODY></HTML>')
+        fid.write(f'\n<br>{footer}<p style="color:#D1D1D1">{errors}</p>\n</BODY></HTML>')
+    if qccols:
+        LOGGER.info(' ')
+        LOGGER.info('To store QC ratings, open:')
+        LOGGER.info(qcfile)
     LOGGER.info(' ')
-    LOGGER.info(f"To view the report, point your web browser at:\n\n{report}\n ")
+    LOGGER.info('To view the slice report, point your web browser at:')
+    LOGGER.info(f"{report}\n ")
 
 
 def main():
@@ -197,10 +259,12 @@ examples:
     parser.add_argument('pattern',                  help="Globlike search pattern to select the images in bidsdir to be reported, e.g. 'anat/*_T2starw*'")
     parser.add_argument('-o','--outlinepattern',    help="Globlike search pattern to select red outline images that are projected on top of the reported images (i.e. 'outlinepattern' must yield the same number of images as 'pattern'. Prepend `outlinedir:` if your outline images are in `outlinedir` instead of `bidsdir` (see examples below)`")
     parser.add_argument('-i','--outlineimage',      help='A common red-outline image that is projected on top of all images')
-    parser.add_argument('-p','--participant_label', help='Space separated list of sub-# identifiers to be processed (the sub- prefix can be left out). If not specified then all sub-folders in the bidsfolder will be processed', nargs='+')
+    parser.add_argument('-p','--participant_label', help='Space separated list of sub-# identifiers to be processed (the sub-prefix can be left out). If not specified then all sub-folders in the bidsfolder will be processed', nargs='+')
     parser.add_argument('-r','--reportfolder',      help="The folder where the report is saved (default: bidsfolder/derivatives/slicereport)")
+    parser.add_argument('-q','--qcscores',          help="Column names for creating an accompanying tsv-file to store QC-rating scores (default: rating_overall)", default=['rating_overall'], nargs='+')
     parser.add_argument('--mainopts',               help='Main options of slicer (see below). (default: "s 1")', default=['s','1'], nargs='+')
     parser.add_argument('--outputopts',             help='Output options of slicer (see below). (default: "x 0.4 x 0.5 x 0.6 y 0.4 y 0.5 y 0.6 z 0.4 z 0.5 z 0.6")', default=['x','0.4','x','0.5','x','0.6','y','0.4','y','0.5','y','0.6','z','0.4','z','0.5','z','0.6'], nargs='+')
+    parser.add_argument('--suboutputopts',          help='Output options of slicer for creating the subreports (same as OUTPUTOPTS, see below). (default: "S 4 1600")', default=['S', '4', '1600'], nargs='+')
     args = parser.parse_args()
 
     slicereport(bidsdir        = args.bidsfolder,
@@ -208,9 +272,11 @@ examples:
                 outlinepattern = args.outlinepattern,
                 outlineimage   = args.outlineimage,
                 subjects       = args.participant_label,
+                reportdir      = args.reportfolder,
+                qccols         = args.qcscores,
                 mainopts       = args.mainopts,
                 outputopts     = args.outputopts,
-                reportdir      = args.reportfolder)
+                suboutputopts  = args.suboutputopts)
 
 
 if __name__ == '__main__':
