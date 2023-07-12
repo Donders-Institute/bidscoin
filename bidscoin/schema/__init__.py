ab4da43dedd2fbf162e81aca5e12c2c30e6e78aa
Marcel Zwiers <m.zwiers@donders.ru.nl>
2023-07-12 10:18:34 +0200

    Add a reference for the schema files
    The CC-BY-4.0 license requires that a reference to the source is given. The CC license is very permissive and permits distributing the entire repository using the GPLv3 license

---
+++ bidscoin/schema/__init__.py
@@ -0,0 +1,8 @@
+"""
+All files in this schema directory and below were copied from the freely distributed BIDS specification repository.
+The original files are released with a Creative Commons Attribution 4.0 International Public (CC-BY-4.0) License. 
+
+For more information see:
+https://github.com/bids-standard/bids-specification
+https://github.com/bids-standard/bids-specification/blob/master/LICENSE
+"""
