2546472aa057bac7ac84abef7307f98a9a8407e8
MarsMellow <mpzwiers@gmail.com>
2023-09-23 11:22:53 +0200

    Revert "Move setup() code to pyproject.toml (https://github.com/praiskup/argparse-manpage/issues/85)"
    This reverts commit bc5e9201d817e8b26a20d9c4d9996a8784799129.

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
 from setuptools import setup
+from build_manpages import build_manpages, get_build_py_cmd, get_install_cmd
 
-setup()
+setup(cmdclass = {'build_manpages': build_manpages,
+                  'build_py': get_build_py_cmd(),
+                  'install': get_install_cmd()})
