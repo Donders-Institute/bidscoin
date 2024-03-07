1b3984ea5e92a812487670840a111fd043bfe973
Marcel Zwiers <m.zwiers@donders.ru.nl>
2024-03-07 17:47:58 +0100

    Bugfix

--- bidscoin/bidsapps/slicereport.py
+++ bidscoin/bidsapps/slicereport.py
@@ -92,13 +92,14 @@ def slicer_append(inputimage: Path, operations: str, outlineimage: Path, mainopt
     workdir.mkdir()
     inputimg = nib.load(inputimage)
     reorient = ''
+    fslext   = '.nii' if os.getenv('FSLOUTPUTTYPE') == 'NIFTI' else '.nii.gz'
     if '.nii' not in inputimage.suffixes:                               # Convert the input image to NIfTI
-        inputimage = workdir/inputimage.with_suffix('').with_suffix('.nii').name
+        inputimage = workdir/inputimage.with_suffix('').with_suffix(fslext).name
         reorient  += f"fslreorient2std {inputimage} {inputimage}\n"
         nib.save(inputimg, inputimage)
     if outlineimage.name and '.nii' not in outlineimage.suffixes:       # Convert the outline image to NIfTI
         outlineimg   = nib.load(outlineimage)
-        outlineimage = workdir/outlineimage.with_suffix('').with_suffix('.nii').name
+        outlineimage = workdir/outlineimage.with_suffix('').with_suffix(fslext).name
         reorient    += f"fslreorient2std {outlineimage} {outlineimage}\n"
         nib.save(outlineimg, outlineimage)
     mathsimg = f"fslmaths {inputimage} {operations} mathsimg\n" if not (len(inputimg.header.get_data_shape())==3 and operations.strip()=='-Tmean') else ''
