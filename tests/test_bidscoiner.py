6ca2483d967d6c8b2ce534435e076f143050c6e2
Marcel Zwiers <m.zwiers@donders.ru.nl>
2025-02-05 00:26:24 +0100

    Minor tweak

--- tests/test_bidscoiner.py
+++ tests/test_bidscoiner.py
@@ -114,7 +114,6 @@ def test_bidscoiner_neurobs(bids_neurobs, bidsmap_neurobs, test_data):
 
     for handler in logging.getLogger().handlers:
         handler.close()
-    bcoin.setup_logging()
     (bidsmap_neurobs.parent/'bidscoiner.log').unlink(missing_ok=True)
     bidscoiner.bidscoiner(testdata, bids_neurobs)
     logs = (bidsmap_neurobs.parent/'bidscoiner.log').read_text()
